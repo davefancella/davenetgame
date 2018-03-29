b23f4b12742861539f4ac0d4a263445ceb464016
Dave Fancella <davefancella@gmail.com>
2018-03-29 07:41:07 -0500

    Minor work on setup.py.  Still can't build from it, though.

--- setup.py
+++ setup.py
@@ -2,40 +2,60 @@
 
 '''
 
-    This file is part of Dave's Stupid Network Game Library.
+   Copyright 2016 Dave Fancella
 
-    Dave's Stupid Network Game Library is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
 
-    Dave's Stupid Network Game Library is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+     http://www.apache.org/licenses/LICENSE-2.0
 
-    You should have received a copy of the GNU General Public License
-    along with Dave's Stupid Network Game Library; if not, write to the Free Software
-    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  00111-1307  USA
-
-    Dave's Stupid Network Game Library is copyright 2016 by Dave Fancella
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
 
 '''
 
-#!/usr/bin/env python3
-
 from distutils.core import setup
 
-setup(name="davenetgame",
-      version='1.0',
-      description="Dave's Stupid Network Game Library is a network library designed for games, written in pure python.",
-      author='Dave Fancella',
-      author_email='dave@davefancella.com',
-      url='http://www.davefancella.com/',
-      packages=[
-                'davenetgame',
-                'davenetgame/messages',
-                ],
-     )
+setup(  name="davenetgame",
+        version='0.1.0.dev0',
+        description="Dave's Stupid Network Game Library is a network library designed for games, written in pure python.",
+        author='Dave Fancella',
+        author_email='dave@davefancella.com',
+        url='http://www.davefancella.com/',
+        license="Apache 2.0",
+        packages=[
+                    'davenetgame',
+                    'davenetgame/dispatch',
+                    'davenetgame/gameobjects',
+                    'davenetgame/messages',
+                    'davenetgame/protocol',
+                    'davenetgame/transport',
+                    ],
+        classifiers=[
+            # How mature is this project? Common values are
+            #   3 - Alpha
+            #   4 - Beta
+            #   5 - Production/Stable
+            'Development Status :: 3 - Alpha',
+
+            # Indicate who your project is intended for
+            'Intended Audience :: Developers',
+
+            # Pick your license as you wish (should match "license" above)
+            'License :: OSI Approved :: Apache License',
+
+            # Specify the Python versions you support here. In particular, ensure
+            # that you indicate whether you support Python 2, Python 3 or both.
+            'Programming Language :: Python :: 3',
+            'Programming Language :: Python :: 3.2',
+            'Programming Language :: Python :: 3.3',
+            'Programming Language :: Python :: 3.4',
+        ],
+        python_requires='>=3',
+)
 
 
